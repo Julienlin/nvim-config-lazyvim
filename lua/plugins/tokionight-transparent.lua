5d45401ca2beb9160785ac4b99141ba5266612cc
Julien Lin <julien.l@padoa-group.com>
2026-03-26 10:31:28 +0100

    update config

--- lua/plugins/tokionight-transparent.lua
+++ lua/plugins/tokionight-transparent.lua
@@ -1,10 +1,11 @@
 return {
   "folke/tokyonight.nvim",
   opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
+    -- style= "day",
+  --   transparent = true,
+  --   styles = {
+  --     sidebars = "transparent",
+  --     floats = "transparent",
+  --   },
   },
 }
